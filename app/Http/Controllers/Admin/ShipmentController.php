cdc92b96884151e87172e43f4e29e5bce13f193c
naandptr <nandaaputr07@gmail.com>
2025-12-18 14:40:37 +0700

    add validation for order and shipment

--- app/Http/Controllers/Admin/ShipmentController.php
+++ app/Http/Controllers/Admin/ShipmentController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Order;
 use App\Models\Shipment;
+use App\Models\StatusHistory;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ShipmentController extends Controller
 {
@@ -31,6 +33,8 @@ class ShipmentController extends Controller
 
     public function store(Request $request, $orderId)
     {
+        $order = Order::findorFail($orderId);
+
         $request->validate([
             'service_type' => 'required|in:delivery,pickup',
             'courier_service' => 'nullable|string',
@@ -40,21 +44,45 @@ class ShipmentController extends Controller
             'shipment_notes' => 'nullable|string',
         ]);
 
-        $shipment = Shipment::create([
-            'order_id' => $orderId,
-            'service_type' => $request->service_type,
-            'courier_service' => $request->courier_service,
-            'shipment_date' => $request->shipment_date,
-            'tracking_number' => $request->tracking_number,
-            'cost_type' => $request->cost_type,
-            'shipment_notes' => $request->shipment_notes,
-        ]);
+        $latestStatus = $order->statusHistory()
+            ->latest('created_at')
+            ->first();
+
+        if ($latestStatus && $latestStatus->status_stage === 'ready') {
+            $shipment = Shipment::create([
+                'order_id' => $orderId,
+                'service_type' => $request->service_type,
+                'courier_service' => $request->courier_service,
+                'shipment_date' => $request->shipment_date,
+                'tracking_number' => $request->tracking_number,
+                'cost_type' => $request->cost_type,
+                'shipment_notes' => $request->shipment_notes,
+            ]);
+
+            StatusHistory::where('order_id', $order->id)
+                ->whereNull('end_time')
+                ->update([
+                    'end_time' => now()
+                ]);
+
+            StatusHistory::create([
+                'order_id' => $order->id,
+                'status_stage' => 'completed',
+                'updated_by' => Auth::id(),
+                'start_time' => now(),
+                'end_time' => now()
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Shipment created successfully',
+                'data' => $shipment
+            ]);
+        }
 
         return response()->json([
-            'success' => true,
-            'message' => 'Shipment created successfully',
-            'data' => $shipment
-        ]);
+            'success' => false, 
+            'message' => 'Order is not yet in ready stage!'], 400);
     }
 
     public function update(Request $request, $shipmentId)
