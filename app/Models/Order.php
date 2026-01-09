3c78567e3cf6bd9f20c1156a5d2f09a60e1793e9
naandptr <nandaaputr07@gmail.com>
2026-01-09 11:05:26 +0700

    update order number generate

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -28,10 +28,10 @@ class Order extends Model
             ->first();
 
         $next = $lastOrder
-            ? intval(substr($lastOrder->order_number, -5)) + 1
+            ? intval(substr($lastOrder->order_number, -3)) + 1
             : 1;
 
-        $counter = str_pad($next, 5, '0', STR_PAD_LEFT);
+        $counter = str_pad($next, 3, '0', STR_PAD_LEFT);
 
         return $prefix . $date . "-" . $counter;
     }
